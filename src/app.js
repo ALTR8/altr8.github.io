e0e88922535753e00a2949ad1d050b8ccf281d3a
Alexandra Roth <alexandra.roth@vaynermedia.com>
2018-01-02 17:14:35 -0500

    I kinda have a site coming along...

---
+++ src/app.js
@@ -0,0 +1,13 @@
+(function($){
+
+    // $('.carousel').slick({
+    //     dots: false,
+    //     arrows: false,
+    //     infinite: true,
+    //     autoplay: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // });
+
+    console.log("Hello World!");
+})(window.jQuery || {});
