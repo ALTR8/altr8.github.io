be5f135b106706e9632d9496f137df3b98d358a9
alexandra.roth <alexandra.roth@vaynermedia.com>
2018-07-10 10:09:20 -0400

    weird version

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 (function($){
 
-    // $('.carousel').slick({
-    //     dots: false,
-    //     arrows: false,
-    //     infinite: true,
-    //     autoplay: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // });
+    $(".nav-anchors").click(function() {
+        target = $(this).attr("href").slice(1);
+        target = $("#" + target);
+        scrollT = target.offset().top-50;
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: scrollT
+            }, 500);
+        }
+        return false;
+    });
+
+    $(document).ready(function () {
+        $(document).click(function (event) {
+            var clickover = $(event.target);
+            var _opened = $(".collapse").hasClass("collapse show");
+            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+                $("button.navbar-toggler").click();
+        }
+    });
+});
+
 
     console.log("Hello World!");
 })(window.jQuery || {});
